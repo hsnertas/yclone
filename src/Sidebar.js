994b70524b3bb3be01eab29c94e4fa49e1c8f592
hsnertas <hsnertas@gmail.com>
2020-09-16 20:41:54 +0300

    first part of sideRow added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./Sidebar.css";
-
+import SidebarRow from "./SidebarRow"
+import HomeIcon from '@material-ui/icons/Home';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
 function Sidebar() {
     return (
         <div className="sidebar">
-        <h1>I am sidebar</h1>
+        <SidebarRow Icon={HomeIcon} title="Home"/>
+        <SidebarRow Icon={WhatshotIcon} title="Trending"/>
+        <SidebarRow Icon={SubscriptionsIcon} title="Subscription"/>
+
         </div>
     )
 }
