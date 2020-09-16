1adc771b027971a9e81f7ff41e1c219edaa30a24
hsnertas <hsnertas@gmail.com>
2020-09-16 12:47:49 +0300

    react setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Let build youtube clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD5GFjVFW_HYk4I-2uDT9Yhk9ldgfuNlbg",
+    authDomain: "clone-ef3fc.firebaseapp.com",
+    databaseURL: "https://clone-ef3fc.firebaseio.com",
+    projectId: "clone-ef3fc",
+    storageBucket: "clone-ef3fc.appspot.com",
+    messagingSenderId: "981064087361",
+    appId: "1:981064087361:web:6a1b4d6182428dc927d3c0",
+    measurementId: "G-QL2XX82QEL"
+  };
