6da29d0bfa38786f535de4b434c5a49bf87ab040
hsnertas <hsnertas@gmail.com>
2020-09-16 22:26:55 +0300

    side finished

--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./SidebarRow.css";
-function SidebarRow({Icon, title}) {
+function SidebarRow({Icon, title, selected}) {
     return (
-        <div className="sidebarRow">
-        <Icon className="sidebarRow__icon"/>
+        <div className={`sidebarRow ${selected && "selected"}`}>
+        <Icon className={"sidebarRow__icon" }/>
             <h2 className="sidebarRow__title">{title}</h2>
         </div>
     )
