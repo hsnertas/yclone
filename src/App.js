d07e100bf11ba0a8f3a05273e660e69ef5f6d474
hsnertas <hsnertas@gmail.com>
2020-09-17 14:56:10 +0300

    search router added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import Header from './Header';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import "./App.css";
 import Sidebar from "./Sidebar";
 import RecommendedVideos from "./RecommendedVideos";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     // BEM class naming
-    
+
     <div className="qpp">
-       {/* Header Component-1 */}
-      <Header/>
-   
-    <div className="app__page">
-    {/* Sidebar Component-2 */}
-    <Sidebar/>
-    {/* RecommendedVideo Component-3 */}
-    <RecommendedVideos/>
-    </div>
-    
+      <Router>
+        <Switch>
+          <Route path="/search">
+          <Header />
+          </Route>
+          <Route path="/">
+            {/* Header Component-1 */}
+            <Header />
+            <div className="app__page">
+              {/* Sidebar Component-2 */}
+              <Sidebar />
+              {/* RecommendedVideo Component-3 */}
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Header.css";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
@@ -6,8 +6,11 @@ import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Avatar from "@material-ui/core/Avatar";
+import {Link} from "react-router-dom";
+
 // I use ES7 snippets
 function Header() {
+  const [inputSearch, setInputSearch]=useState('');
   return (
     <div className="header">
       <div className="header__left">
@@ -19,8 +22,11 @@ function Header() {
         />
       </div>
       <div className="header__input">
-        <input placeholder="Search" type="text" />
+        <input onChange={e=>setInputSearch(e.target.value)} value={inputSearch} placeholder="Search" type="text" />
+        <Link to={`/search/${inputSearch}`}>
         <SearchIcon className="header__inputButton"/>
+        </Link>
+     
       </div>
       <div className="header__icons">
         <VideoCallIcon className="header_icon"/>
@@ -34,5 +40,6 @@ function Header() {
     </div>
   );
 }
+
 
 export default Header;
