7688236a90abe7b886ff8e53196ddb831f067bbb
hsnertas <hsnertas@gmail.com>
2020-09-28 16:21:48 -0400

    npm build added

---
+++ build/precache-manifest.cd4a035071f4ac1fbf097646f3e2f1b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1cea46f2cb31ba9af87d5eed741224",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72b0c44c112d7970938a",
+    "url": "/static/css/main.5b952bc1.chunk.css"
+  },
+  {
+    "revision": "450f68aac981875eb74e",
+    "url": "/static/js/2.c081ad02.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c081ad02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b0c44c112d7970938a",
+    "url": "/static/js/main.6997f4b3.chunk.js"
+  },
+  {
+    "revision": "f5ed15b071ecd5efa918",
+    "url": "/static/js/runtime-main.db20ac27.js"
+  }
+]);
