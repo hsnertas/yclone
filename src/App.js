7e9693c001c1054031d4fd12f7b1d973a44fa7f0
hsnertas <hsnertas@gmail.com>
2020-09-28 13:30:51 -0400

    search page channel section finished

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import "./App.css";
 import Sidebar from "./Sidebar";
 import RecommendedVideos from "./RecommendedVideos";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
 function App() {
   return (
     // BEM class naming
 
     <div className="qpp">
       <Router>
+      <Header />
         <Switch>
           <Route path="/search/:searchTerm">
-          <Header />
+          <div className="app__page">
           <Sidebar />
-          
+          <SearchPage />
+          </div>
           </Route>
           <Route path="/">
             {/* Header Component-1 */}
-            <Header />
+           
             <div className="app__page">
               {/* Sidebar Component-2 */}
               <Sidebar />
